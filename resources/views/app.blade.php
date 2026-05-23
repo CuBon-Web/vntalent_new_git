aad1f3bbbe95368b1d3287c7b1ad4385e0a9c7c8
iip-tuananh <tuananhdinamo1122@gmai.com>
2026-05-23 08:10:24 +0700

    first commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script>
-      window.__ENV__ = window.__ENV || {}; __ENV__.link ='https://vntalent.brandvn.com.vn/';
+      window.__ENV__ = window.__ENV || {}; __ENV__.link ='https://vntalenthub.com/';
       window.Laravel = {!! 
           json_encode([
               'csrf_token' => csrf_token(),
